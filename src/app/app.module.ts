b30df423bc99931f670e2450c7b80455e770d379
Camilo Velasquez <camilo.velasquezp@gmail.com>
2019-03-30 02:44:21 -0500

    Se consume api de menu, se parametriza mensaje segun el menu que se elija

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AirbusComponent } from './airbus/airbus.component';
+import { BoeingComponent } from './boeing/boeing.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AirbusComponent,
+    BoeingComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/data.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Menu } from './menu';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  apiUrl = "http://localhost:8000/my-service/";
+
+  constructor(private clientHttp: HttpClient) { }
+
+  getMenus() {
+    return this.clientHttp.get<Menu[]>(this.apiUrl + 'menus/');
+  }
+}
