8356cd1938cabebd7262d7757c22f90b69f0e6c3
Camilo Velasquez <camilo.velasquezp@gmail.com>
2019-03-30 23:20:29 -0500

    Se crea formulario, con sus validaciones, se hacen ajustes a menu responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Menu } from './menu';
+import { Menu } from './menu.model';
 import { DataService } from './data.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataForm } from './data-form.model';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +11,24 @@ import { DataService } from './data.service';
 })
 export class AppComponent implements OnInit {
   title = 'menu';
-  isChecked = true;
+  isChecked = false;
   menus: Menu[];
   isHidden = true;
+  travelIn = '';
 
-  constructor(private service: DataService) { }
+  registerForm: FormGroup;
+  submitted = false;
+
+  constructor(private service: DataService, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
+    this.registerForm = this.formBuilder.group({
+      fullName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      celPhone: ['', Validators.required],
+      travelIn: ['', Validators.required],
+      age: ['', [Validators.required, Validators.min(18), Validators.max(100)]]
+    });
     this.service.getMenus().subscribe((data) => { this.menus = data });
   }
 
@@ -26,4 +39,19 @@ export class AppComponent implements OnInit {
   public setIsHidden(valuee: boolean) {
     this.isHidden = valuee;
   }
+
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    }
+    let dataForm: DataForm;
+    dataForm = this.registerForm.value;
+    alert('SUCCESS!! :-)' + JSON.stringify(dataForm));
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DataService } from './data.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AirbusComponent } from './airbus/airbus.component';
 import { BoeingComponent } from './boeing/boeing.component';
 import { HomeComponent } from './home/home.component';
@@ -21,7 +21,8 @@ import { HomeComponent } from './home/home.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
